82e7e81780f6006a16575d4e576202df2155478d
rahul sharma <111222699+rahulsharma8373@users.noreply.github.com>
2022-09-29 15:08:32 +0530

    Add files via upload

---
+++ ngopartner/src/main/java/com/ngo/ngopartner/NgopartnerApplication.java
@@ -0,0 +1,13 @@
+package com.ngo.ngopartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgopartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgopartnerApplication.class, args);
+	}
+
+}
---
+++ ngopartner/src/test/java/com/ngo/ngopartner/NgopartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngo.ngopartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgopartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
